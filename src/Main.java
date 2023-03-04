13e3f949794f3747effa4bd21337df9844d0b22e
Weoga <iliazel8@gmail.com>
2023-03-04 11:22:25 +0200

    changed a little bit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Type;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 //        System.out.println("Hello World! :)");
@@ -19,18 +22,46 @@ public class Main {
 //        String my_str = "O, haven't seen you before";
 //
 //        System.out.println(my_str);
-        float a = 10;
-        float b = 3;
-        float c = a / b;
-        float d = a + b;
-        float e = a - b;
-        float f = a * b;
-        float h = a % b;
+//        float a = 3;
+//        float b = 10;
+//        float c = a / b;
+//        float d = a + b;
+//        float e = a - b;
+//        float f = a * b;
+//        float h = a % b;
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(h);
 
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(h);
+
+        String cursed = "34";
+        int cursed_num = Integer.parseInt(cursed);
+
+//        float cursed_math = cursed_num / a;
+
+//        System.out.println(cursed_math);
+
+//        int victim = 435345;
+//        String wtf = String.valueOf(victim);
+//        System.out.println(wtf.chars());
+
+        int a = 34;
+        int b = 4662;
+
+
+//        if(a > b){
+//            System.out.println(true);
+//        }else if(a == b){
+//            System.out.println("trying to scam me??");
+//        }else {
+//            System.out.println("aaaaaaaaaaa");
+//        }
+
+//        String answer = a>b ? "a>b" : "a<b";
+//        System.out.println(answer);
+
+
     }
 }
