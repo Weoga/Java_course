2cda218d9fccdfebc55b43d0c77ad13f4a25f2e6
Weoga <iliazel8@gmail.com>
2023-03-04 10:33:58 +0200

    Initial commit originally failed..

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+//        System.out.println("Hello World! :)");
+//        int number = 11;
+//        System.out.println(number);
+//        number = 0;
+//        System.out.println(number);
+//
+//        float dot = (float) 1.43;
+//        char letter = 'a';
+//        double dot2 = 3.14;
+//        boolean tr = true;
+//
+//        System.out.println(dot);
+//        System.out.println(letter);
+//        System.out.println(dot2);
+//        System.out.println(tr);
+//
+//        String my_str = "O, haven't seen you before";
+//
+//        System.out.println(my_str);
+        float a = 10;
+        float b = 3;
+        float c = a / b;
+
+        System.out.println(c);
+
+    }
+}
