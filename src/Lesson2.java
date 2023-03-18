3a271986563f38397969d45284f868a61f66240e
Weoga <iliazel8@gmail.com>
2023-03-18 10:01:49 +0200

    second mid lesson 2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,9 +1,7 @@
-import java.io.Console;
-import java.util.Arrays;
-
+import java.util.Scanner;
 public class Lesson2 {
 
-    public void main(){
+    public void arrays(){
         int arr1[] = new int[10];
 
         int arr2[];
@@ -19,11 +17,43 @@ public class Lesson2 {
         arr1[8] = arr1[0] * 4;
         arr1[3] = arr1[3] - 5;
 
-        System.out.println("lol");
+//        System.out.println("lol");
 
-        for (int num: arr1
-             ) {
-            System.out.println(num);
+//        for (int num: arr1
+//             ) {
+//            System.out.println(num);
+//        }
+//        System.out.println("Second way");
+//
+//        for (int i = 0; i < arr1.length; i++){
+//            System.out.println(arr1[i]);
+//        }
+
+//        System.out.println("lenght");
+//        System.out.println(arr1.length);
+
+        Scanner input_scan = new Scanner(System.in);
+
+        int numbers[] = new int[5];
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Input number n" + (i+1));
+            boolean not_parsed = true;
+            while (not_parsed) {
+                try {
+                    String input = input_scan.next();
+                    int input_fancy = Integer.parseInt(input);
+                    numbers[i] = input_fancy;
+                    not_parsed = false;
+                } catch (java.lang.NumberFormatException e) {
+                    System.out.println("I NEED NUMBERS");
+                }
+            }
         }
+        for (int number: numbers
+            ) {
+            System.out.println(number);
+                }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import java.lang.reflect.Type;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
         Lesson2 My_obj = new Lesson2();
 
-        My_obj.main();
+        My_obj.arrays();
     }
 }
